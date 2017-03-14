5aa6d765e2c78cf21247f4c760bb71edcd7669e0
jfdesrochers <jfdesrochers@gmail.com>
2017-03-13 20:56:05 -0400

    Completed email updates

--- server/mailupdates.js
+++ server/mailupdates.js
@@ -2,13 +2,14 @@ const {summarize} = require('./utils/sum.js')
 const {Database} = require('./data.js')
 const {getWeekNo, getAllWeeks} = require('../assets/js/utils/dateutils.js')
 const Mustache = require('mustache')
+const sendMail = require('./sendmail.js')
+const env = require('../env.json')
 const fs = require('fs')
+const path = require('path')
 
 // load and parse template
-const template = fs.readFileSync('./templates/email.html', 'utf8')
-Mustache.parse(template)
-const templateText = fs.readFileSync('./templates/email.txt', 'utf8')
-Mustache.parse(templateText)
+const template = fs.readFileSync(path.join(__dirname, 'templates', 'email.html'), 'utf8')
+const templateText = fs.readFileSync(path.join(__dirname, 'templates', 'email.txt'), 'utf8')
 
 // initalize email cache and variables
 const emailCache = {'districts': {}, 'stores': {}}
@@ -21,24 +22,29 @@ Database.emailGetUsers()
     let promise = users.reduce((pms, user) => {
         return pms.then(() => {
             if (emailCache['districts'][user._id.district] !== undefined) {
+                emailCache['districts'][user._id.district].emails.push.apply(emailCache['districts'][user._id.district].emails, user.emails)
                 return
             }
-            emailCache['districts'][user._id.district] = []
-            return Database.getHBs({district: user._id.district, week: curWeek})
+            emailCache['districts'][user._id.district] = {sales: [], serv: [], dev: [], emails: user.emails}
+            return Database.getHBs({district: user._id.district, week: curWeek, status: 'published'})
             .then((hbs) => {
-                return Database.emailGetStores({district: user._id.district, week: curWeek})
-                .then((stores) => {
-                    hbs.filter((o) => {
-                        return (stores.indexOf(o.store) === -1)
-                    }).map((o) => {
-                        emailCache['districts'][user._id.district].push({store: o.store, summary: summarize([o.salesCom, o.servCom, o.devCom]).join(' [...] ')})
-                        stores.push(o.store)
-                    })
-                    Database.emailSetStores({district: user._id.district, week: curWeek, storeList: stores})
+                hbs.sort((p, n) => {
+                    return parseInt(p.store) - parseInt(n.store)
+                })
+                hbs.map((o) => {
+                    if (o.salesCom.length > 0) {
+                        emailCache['districts'][user._id.district].sales.push({store: o.store, summary: summarize(o.salesCom)})
+                    }
+                    if (o.servCom.length > 0) {
+                        emailCache['districts'][user._id.district].serv.push({store: o.store, summary: summarize(o.servCom)})
+                    }
+                    if (o.devCom.length > 0) {
+                        emailCache['districts'][user._id.district].dev.push({store: o.store, summary: summarize(o.devCom)})
+                    }
                 })
             })
         })
-        .then(() => {
+        /*.then(() => {
             return Database.getHBs({district: user._id.district, store: user._id.store, week: curWeek, lastupdate: yesterday})
             .then((hbs) => {
                 if (hbs.length > 0) {
@@ -49,10 +55,22 @@ Database.emailGetUsers()
                     })
                 }
             })
-        })
+        })*/
     }, Promise.resolve())
     promise.then(() => {
-        console.log(emailCache)
+        Object.keys(emailCache.districts).forEach((o) => {
+            let email = emailCache.districts[o]
+            email['week'] = getAllWeeks()[parseInt(curWeek)-1][1]
+            let html = Mustache.render(template.toString(), email)
+            let text = Mustache.render(templateText.toString(), email)
+            sendMail({
+                from: '"How\'s Biz par J-F Desrochers" <' + env.GMAILADDR + '>',
+                to: emailCache.districts[o].emails.join(','), 
+                subject: 'Vos How\'s Biz de la semaine ' + curWeek,
+                text: text, 
+                html: html
+            })
+        })
     })
 })
 .catch(function (err) {
