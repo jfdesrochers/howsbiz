1e894d572af5e05ad262ef341f6074491e7f16cd
Jean-Francois Desrochers <jfdesrochers@gmail.com>
2017-03-27 22:24:50 -0400

    Added autopublish goodness

--- server/autopublish.js
+++ server/autopublish.js
@@ -3,4 +3,5 @@ const {Database} = require('./data.js')
 
 const curWeek = String(getWeekNo(true))
 
+console.log('Publishing all unpublished How\'s biz from week %s', curWeek)
 Database.publishAllHB(curWeek).then(() => console.log('All Done!'))
