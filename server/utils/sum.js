120cfbc9fa374d2805a6d2c31c00d7061a142f8f
Jean-Francois Desrochers <jfdesrochers@gmail.com>
2017-04-03 22:27:03 -0400

    Don't summarize, for some reason...

--- server/utils/sum.js
+++ server/utils/sum.js
@@ -1,9 +1,9 @@
 const h2p = require('html2plaintext')
 
 const summarize = function(o) {
-    o = h2p(o.replace(/<h[1-6]>.+<\/h[1-6]>/gi, '')).replace(/\n/gi, ' ')
-    let trimmedString = o.substr(0, 140);
-    trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" "))) + '...'
+    o = h2p(o.replace(/<h[1-6]>.+<\/h[1-6]>/gi, ''))//.replace(/\n/gi, ' ')
+    let trimmedString = o//.substr(0, 140);
+    //trimmedString = trimmedString.substr(0, Math.min(trimmedString.length, trimmedString.lastIndexOf(" "))) + '...'
     return trimmedString
 }
 
